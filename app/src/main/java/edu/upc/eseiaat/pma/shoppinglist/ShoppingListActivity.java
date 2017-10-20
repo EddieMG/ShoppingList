8fb7f0294ffa0847b98a3a84d110c5333cb4fbdd
Eddie <eduardmgobierno@gmail.com>
2017-10-20 13:40:37 +0200

    Commit 2

--- app/src/main/java/edu/upc/eseiaat/pma/shoppinglist/ShoppingListActivity.java
+++ app/src/main/java/edu/upc/eseiaat/pma/shoppinglist/ShoppingListActivity.java
@@ -31,7 +31,11 @@ public class ShoppingListActivity extends AppCompatActivity {
         itemlist.add("iPrusa");
         itemlist.add("Snowboard");
 
-        adapter= new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,itemlist);
+        adapter= new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                itemlist
+        );
         list.setAdapter(adapter);
     }
 }
